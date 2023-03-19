fb74b81a94be9a7950f072c8b28ae83e7b1c0076
Christian Bager Bach Houmann <christian@bagerbach.com>
2023-03-19 19:39:27 +0100

    .

--- lecture7/main.cpp
+++ lecture7/main.cpp
@@ -1,52 +1,71 @@
-#include <vector>
 #include <iostream>
-#include <typeinfo>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <type_traits>
+#include <list>
+#include <deque>
+#include <set>
 
-void print(std::vector<int> v)
-{
-    for (int i = 0; i < v.size(); i++)
-    {
-        std::cout << v[i] << " ";
+// Overload the output operator for std::vector<T>
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
+    os << '{';
+    for (size_t i = 0; i < vec.size(); ++i) {
+        if constexpr (std::is_same_v<T, const char*>)
+            os << std::quoted(vec[i]);
+        else
+            os << vec[i];
+        if (i < vec.size() - 1) os << ',';
     }
-
-    std::cout << std::endl;
-}
-
-// std::ostream &operator<<(std::ostream &os, const std::vector<int> &v)
-// {
-//     for (int i = 0; i < v.size(); i++)
-//     {
-//         os << v[i] << " ";
-//     }
-
-//     return os;
-// }
-
-template <typename T>
-std::ostream &operator<<(std::ostream &os, const std::vector<T> &v)
-{
-    for (int i = 0; i < v.size(); i++)
-    {
-        os << v[i] << " ";
-    }
-
+    os << '}';
     return os;
 }
 
-template <typename T>
-class TD;
+/*
+This template function overloads the output operator for std::vector<T> where T is any printable type.
+It iterates through the vector elements and prints them inside curly braces in a comma-separated format.
+If the element type T is const char*, it uses std::quoted to add quotes around the element.
+*/
 
-template<typename T>
-void type_displayer(const T&) {
-    std::cout << typeid(T).name() << std::endl;
+// Generalized output operator for any container (excluding std::string)
+template <typename C, typename T = typename C::value_type, typename std::enable_if<!std::is_same<C, std::string>::value, int>::type = 0>
+std::ostream& operator<<(std::ostream& os, const C& container) {
+    os << '{';
+    auto it = container.begin();
+    for (; it != container.end(); ++it) {
+        if constexpr (std::is_same_v<T, const char*>)
+            os << std::quoted(*it);
+        else
+            os << *it;
+        if (std::next(it) != container.end()) os << ',';
+    }
+    os << '}';
+    return os;
 }
 
-int main()
-{
-    std::vector<int> vec = {1, 2, 3, 4, 5};
-    print(vec);
-    std::cout << vec << std::endl;
+/*
+This template function overloads the output operator for any container C with element type T, excluding std::string.
+It uses SFINAE with std::enable_if to exclude std::string from this template.
+The function iterates through the container using iterators and prints the elements inside curly braces in a comma-separated format.
+If the element type T is const char*, it uses std::quoted to add quotes around the element.
+*/
 
-    // TD<decltype(vec)> td;
-    // type_displayer(vec);
+int main() {
+    // Test cases for std::vector
+    std::cout << std::vector{1, 2, 3} << std::endl;
+    std::cout << std::vector{2.7, 3.14} << std::endl;
+    std::cout << std::vector{std::vector{"hello", "template"}, std::vector{"world"}} << std::endl;
+
+    // Test cases for generalized output operator
+    std::list<int> intList{1, 2, 3};
+    std::cout << intList << std::endl;
+
+    std::deque<std::string> strDeque{"hello", "world"};
+    std::cout << strDeque << std::endl;
+
+    std::set<double> doubleSet{3.14, 2.718};
+    std::cout << doubleSet << std::endl;
+
+    return 0;
 }
