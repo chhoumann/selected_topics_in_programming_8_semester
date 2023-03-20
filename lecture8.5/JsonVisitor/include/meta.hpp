b347f9c326932bd7a00d237cec89b241a93c61af
Christian Bager Bach Houmann <christian@bagerbach.com>
2023-03-20 12:32:55 +0100

    add concepts

--- lecture8.5/JsonVisitor/include/meta.hpp
+++ lecture8.5/JsonVisitor/include/meta.hpp
@@ -23,16 +23,28 @@ template <typename T>
 constexpr bool is_bool_v = std::is_same_v<bool, std::remove_cvref_t<T>>;
 
 template <typename T>
+concept Boolean = is_bool_v<T>;
+
+template <typename T>
 constexpr bool is_number_v = std::is_arithmetic_v<std::remove_cvref_t<T>> && !is_bool_v<T>;
 
 template <typename T>
+concept Number = is_number_v<T>;
+
+template <typename T>
 constexpr bool is_character_v = std::is_same_v<std::remove_cvref_t<T>, char>;
+
+template <typename T>
+concept Character = is_character_v<T>;
 
 template <typename T>
 constexpr bool is_string_v =
     std::is_same_v<std::remove_cvref_t<T>, std::string> || std::is_same_v<std::remove_cvref_t<T>, char*> ||
     std::is_same_v<std::remove_cvref_t<T>, const char*> ||
     (std::is_array_v<std::remove_cvref_t<T>> && std::is_same_v<std::remove_extent_t<std::remove_cvref_t<T>>, char>);
+
+template <typename T>
+concept String = is_string_v<T>;
 
 template <typename T>
 concept container = requires(T t) {
@@ -42,6 +54,9 @@ concept container = requires(T t) {
 template <typename T>
 // FIXME: use concepts, require std::begin, std::end, and not is_string_v
 constexpr auto is_container_v = (container<T> || std::is_array_v<std::remove_cvref_t<T>>)&&!is_string_v<T>;
+
+template <typename T>
+concept Container = is_container_v<T>;
 
 template <typename Data, typename Visitor>
 // FIXME: check for Data::accept(Visitor) by using concepts or detection toolkit
