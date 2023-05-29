9e663061eef635228de1198df174e0056aa170e5
Christian Bager Bach Houmann <christian@bagerbach.com>
2023-05-29 18:48:05 +0200

    simulator kinda working

--- exam_assignment/src/stochastic_simulator.cpp
+++ exam_assignment/src/stochastic_simulator.cpp
@@ -19,33 +19,25 @@ public:
     }
 
     void simulate() {
-        std::cout << "Simulating..." << std::endl;
         double t = 0;
 
         auto& reactions = system_.getReactions();
-        std::unordered_map<Reaction, double, ReactionHash> reaction_delays;
 
         while (t <= end_time_) {
-            std::cout << "t = " << t << std::endl;
             for (auto &r : reactions) {
                 const auto delay = compute_delay(r);
-                reaction_delays[r] = delay;
+                r.setDelay(delay);
             }
 
-            auto next_reaction = find_min_delay_reaction(reaction_delays);
+            auto next_reaction = find_min_delay_reaction(reactions);
             if (!next_reaction) {
-                std::cout << "No more reactions can proceed" << std::endl;
                 break; // No more reactions can proceed
             }
-            std::cout << "Next reaction: " << *next_reaction << std::endl;
-            t += next_reaction->rate;
-            std::cout << "t = " << t << std::endl;
+            t += next_reaction->delay();
             if (can_react(*next_reaction)) {
-                std::cout << "Reacting..." << std::endl;
                 react(*next_reaction);
             }
 
-            std::cout << "State: " << std::endl;
             print_state();
         }
     }
@@ -56,7 +48,7 @@ private:
     std::default_random_engine generator_;
 
     double compute_delay(const Reaction &r) {
-        double lambda_k = r.rate;
+        double lambda_k = r.rate();
 
         // λk = λ * ∏i Ri,k
         for (const auto &reactant : r.reactants) {
@@ -67,14 +59,14 @@ private:
         return distribution(generator_);
     }
 
-    std::shared_ptr<Reaction> find_min_delay_reaction(std::unordered_map<Reaction, double, ReactionHash> &reaction_delays) {
+    std::shared_ptr<Reaction> find_min_delay_reaction(const std::vector<Reaction> &reactions) {
         double min_delay = std::numeric_limits<double>::max();
         std::shared_ptr<Reaction> min_reaction = nullptr;
 
-        for (auto &[reaction, delay] : reaction_delays) {
-            if (delay < min_delay) {
+        for (auto const &reaction : reactions) {
+            if (reaction.delay() < min_delay) {
                 min_reaction = std::make_shared<Reaction>(reaction);
-                min_delay = delay;
+                min_delay = reaction.delay();
             }
         }
 
@@ -82,41 +74,35 @@ private:
     }
 
     bool can_react(const Reaction &r) {
-        std::cout << "Checking if reaction can proceed..." << std::endl;
-        std::cout << "Reaction: " << r << std::endl;
-        std::cout << "Reactants Size: " << r.reactants.size() << std::endl;
-
         for (size_t i = 0; i < r.reactants.size(); ++i) {
-            std::cout << "What the fuck just do it" << std::endl; //<< r.reactants[i].getName() << " >= " << r.delta_R[i] << std::endl;
             auto species_amount = system_.getAmount(r.reactants.at(i));
-            std::cout << "amount: " << species_amount << std::endl;
 
-            if (species_amount < r.delta_R[i]) {
-                std::cout << "Reaction cannot proceed" << std::endl;
+            // Currently assuming only 1 reactant is needed
+            if (species_amount < 1) {
                 return false;
             }
         }
-        std::cout << "Reaction can proceed" << std::endl;
         return true;
     }
 
     void react(Reaction &r) {
         for (size_t i = 0; i < r.reactants.size(); ++i) {
-            const double amount = system_.getAmount(r.reactants[i]) - r.delta_R[i];
+            // Currently assuming only 1 reactant is consumed
+            const double amount = system_.getAmount(r.reactants[i]) - 1;
             system_.setAmount(r.reactants[i], amount);
         }
 
         for (size_t i = 0; i < r.products.size(); ++i) {
-            const double amount = system_.getAmount(r.products[i]) + r.delta_P[i];
+            // Currently assuming only 1 product is produced
+            const double amount = system_.getAmount(r.products[i]) + 1;
             system_.setAmount(r.products[i], amount);
         }
     }
 
     void print_state() {
-        auto species = system_.getSpecies();
 
-        for (const auto &[s, amount] : species) {
-            std::cout << s.getName() << ": " << amount << " ";
+        for (auto species = system_.getSpecies(); const auto &[s, amount] : species) {
+            std::cout << s.getName() << ": " << amount << " " << std::endl;
         }
 
         std::cout << '\n';
