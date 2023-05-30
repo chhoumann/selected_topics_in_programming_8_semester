10e73144743595e75d6bf67705f80c4d522458be
Christian Bager Bach Houmann <christian@bagerbach.com>
2023-05-30 15:10:14 +0200

    run simulator for circadian

--- exam_assignment/src/main.cpp
+++ exam_assignment/src/main.cpp
@@ -2,17 +2,20 @@
 #include "examples/seihr.cpp"
 #include "graph_generator.cpp"
 #include "stochastic_simulator.cpp"
+#include "examples/circadian_oscillator.cpp"
 #include <iostream>
 
 int main(int argc, char const *argv[])
 {
-    auto v = seihr(100'000);
+    //auto v = seihr(100'000);
+    auto v = circadian_oscillator();
+
     //auto const& reactions = v.getReactions();
 
     //std::string filename = "seihr.png";
     //generate_graph(reactions, filename);
 
-    auto s = Simulator(v, 1000000);
+    auto s = Simulator(v, 100);
     s.simulate();
 
     return 0;
