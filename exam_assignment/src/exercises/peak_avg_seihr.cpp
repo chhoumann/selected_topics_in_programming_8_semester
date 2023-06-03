460b483d795645b82f7ee11991069f4b41cb6f80
Christian Bager Bach Houmann <christian@bagerbach.com>
2023-06-03 14:14:57 +0200

    rename fn

--- exam_assignment/src/exercises/peak_avg_seihr.cpp
+++ exam_assignment/src/exercises/peak_avg_seihr.cpp
@@ -12,7 +12,7 @@ double run_seihr_simulation(size_t N) {
 }
 
 template <typename Func>
-void perform_simulation(const size_t N, const size_t num_simulations, ThreadPool& thread_pool, Func operate_on_results) {
+void perform_parallel_simulations(const size_t N, const size_t num_simulations, ThreadPool& thread_pool, Func operate_on_results) {
     std::vector<std::future<double>> futures;
     std::vector<double> results;
 
@@ -46,14 +46,16 @@ void calculate_peak_and_avg_seihr(size_t num_simulations, size_t concurrency_lev
 
     ThreadPool thread_pool(concurrency_level);
 
-    perform_simulation(N_NJ, num_simulations, thread_pool, [&](std::vector<double> const& results) {
+    perform_parallel_simulations(N_NJ, num_simulations, thread_pool, [&](std::vector<double> const &results) {
         double avg_peak = calculate_mean(results);
-        std::cout << "Average peak of Hospitalized in NJ over " << num_simulations << " simulations: " << avg_peak << std::endl;
+        std::cout << "Average peak of Hospitalized in NJ over " << num_simulations << " simulations: " << avg_peak
+                  << std::endl;
     });
 
-    perform_simulation(N_DK, num_simulations, thread_pool, [&](std::vector<double> const& results) {
+    perform_parallel_simulations(N_DK, num_simulations, thread_pool, [&](std::vector<double> const &results) {
         double max_peak = calculate_peak(results);
-        std::cout << "Maximum peak of Hospitalized in DK over " << num_simulations << " simulations: " << max_peak << std::endl;
+        std::cout << "Maximum peak of Hospitalized in DK over " << num_simulations << " simulations: " << max_peak
+                  << std::endl;
     });
 
     auto end = std::chrono::steady_clock::now();
